e05bbbc53540ee08d5ad52ad0d0189690ff76c92
Lynsie Aragon <lynsie.aragon@gmail.com>
2022-12-21 16:53:53 -0500

    testing uniqueness, incomplete

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,31 @@
 #  password_digest :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#  session_token   :string           not null
 #
 class User < ApplicationRecord
+    # debugger
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    attr_reader :password
+
+    def username=(username)
+        @username = username
+        debugger
+    end
+
+    def is_password?(other_pass)
+        pass_object = BCrypt::Password.new(self.password_digest)
+        pass_object.is_password?(other_pass)
+    end
+
+    def password=(new_password)
+        self.password_digest = BCrypt::Password.create(new_password)
+        @password = new_password
+    end
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_200001) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_205851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_200001) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_token", null: false
     t.index ["username"], name: "index_users_on_username"
   end
 
